6e5aa32c2ad4c7151f841db6d14dfd6088d8367d
ahmmedms <mrmoood@hotmail.com>
2021-01-06 21:59:13 +0300

    meh

---
+++ lib/widgets/w-praying.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Praying extends StatefulWidget {
+  @override
+  _PrayingState createState() => _PrayingState();
+}
+
+class _PrayingState extends State<Praying> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
