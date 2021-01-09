2eeb7662472f5d2897296686920d2cd237bd57c5
ahmmedms <mrmoood@hotmail.com>
2021-01-10 00:23:35 +0300

    made the app available on portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'widgets/w-main-page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -13,6 +21,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold( body: MainPage(), ));
+    return MaterialApp(
+        home: Scaffold(
+      body: MainPage(),
+    ));
   }
 }
--- lib/widgets/w-main-page.dart
+++ lib/widgets/w-main-page.dart
@@ -8,48 +8,51 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
-
-
   @override
   Widget build(BuildContext context) {
-      final appBar = AppBar(
-              backgroundColor: Color.fromRGBO(243, 233, 219, 1),
-              title: Text(
-                ' الأدعية ',
-                style: TextStyle(color: Colors.black, fontSize: 28),
-              ));
+    final appBar = AppBar(
+        backgroundColor: Color.fromRGBO(243, 233, 219, 1),
+        title: Text(
+          ' الأدعية ',
+          style: TextStyle(color: Colors.black, fontSize: 28),
+        ));
 
     final curScaleFactor = MediaQuery.of(context).textScaleFactor;
-    return MaterialApp(
-        theme: ThemeData(
-          primaryColor: Color.fromRGBO(243, 233, 219, 1),
-          accentColor: Color.fromRGBO(236, 106, 92, 1),
-          cardColor: Color.fromRGBO(249, 221, 215, .8),
-          errorColor: Colors.red,
-          textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText1: TextStyle(
-              fontSize: 18*curScaleFactor,
-              color: Color.fromRGBO(209, 68, 51, 1)
-            )
-          )
-        ),
-        builder: (context, child) {
-          return Directionality(textDirection: TextDirection.rtl, child: child);
-        },
-        home: Scaffold(
-          appBar:appBar ,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height:( MediaQuery.of(context).size.height -  (MediaQuery.of(context).padding.top + appBar.preferredSize.height)) *0.8,
-                child: Praying()
-                ),
-              Container(
-                height: ( MediaQuery.of(context).size.height - (MediaQuery.of(context).padding.top + appBar.preferredSize.height ) ) *0.2,
-                child: Counting()),
-            ],
-          ),
-        ));
+    return LayoutBuilder(builder: (ctx, contsraints) {
+      return MaterialApp(
+          theme: ThemeData(
+              primaryColor: Color.fromRGBO(243, 233, 219, 1),
+              accentColor: Color.fromRGBO(236, 106, 92, 1),
+              cardColor: Color.fromRGBO(249, 221, 215, .8),
+              errorColor: Colors.red,
+              textTheme: ThemeData.light().textTheme.copyWith(
+                  bodyText1: TextStyle(
+                      fontSize: 18 * curScaleFactor,
+                      color: Color.fromRGBO(209, 68, 51, 1)))),
+          builder: (context, child) {
+            return Directionality(
+                textDirection: TextDirection.rtl, child: child);
+          },
+          home: Scaffold(
+            appBar: appBar,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    height: (MediaQuery.of(context).size.height -
+                            (MediaQuery.of(context).padding.top +
+                                appBar.preferredSize.height)) *
+                        0.8,
+                    child: Praying()),
+                Container(
+                    height: (MediaQuery.of(context).size.height -
+                            (MediaQuery.of(context).padding.top +
+                                appBar.preferredSize.height)) *
+                        0.2,
+                    child: Counting()),
+              ],
+            ),
+          ));
+    });
   }
 }
