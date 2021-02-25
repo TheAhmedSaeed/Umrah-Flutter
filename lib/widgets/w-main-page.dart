fbc94c553ea3a7c1e72033a91136e4614273bc73
ahmmedms <mrmoood@hotmail.com>
2021-02-26 00:17:34 +0300

    changed the splash screen

--- lib/widgets/w-main-page.dart
+++ lib/widgets/w-main-page.dart
@@ -13,45 +13,54 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    final PreferredSizeWidget  appBar =  Platform.isIOS ? CupertinoNavigationBar(
-      backgroundColor: Color.fromRGBO(109, 131, 182, 1),
-      middle: Text(
-          ' عمرة + ',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-    ) : AppBar(
-        backgroundColor: Color.fromRGBO(109, 131, 182, 1),
-        title: Text(
-          ' عمرة + ',
-          style: TextStyle(color: Colors.black, fontSize: 28),
-        ));
+    final PreferredSizeWidget appBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            backgroundColor: Color.fromRGBO(56, 47, 56, 1),
+            middle: Text(
+              ' عمرة + ',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          )
+        : AppBar(
+            backgroundColor: Color.fromRGBO(56, 47, 56, 1),
+            title: Text(
+              ' عمرة + ',
+              style: TextStyle(color: Colors.black, fontSize: 28),
+            ));
 
+    final actualScreenSize = (mediaQuery.size.height -
+        (mediaQuery.padding.top +
+            appBar.preferredSize.height +
+            mediaQuery.padding.bottom));
+
+    final counterPtg = mediaQuery.size.height < 800 ? 0.27 : 0.2;
+  print("mediaQuery.size.height");
+  print(mediaQuery.size.height);
     final curScaleFactor = mediaQuery.textScaleFactor;
     return LayoutBuilder(builder: (ctx, contsraints) {
-      return 
-      MaterialApp(
+      return MaterialApp(
           theme: ThemeData(
-              primaryColor: Color.fromRGBO(109, 131, 182, 1),
+              primaryColor: Color.fromRGBO(56, 47, 56, 1),
               accentColor: Color.fromRGBO(42, 47, 53, 1),
-              cardColor: Color.fromRGBO(175, 215, 251, 1),
+              cardColor: Color.fromRGBO(228, 179, 138, 1),
               errorColor: Colors.red,
               textTheme: ThemeData.light().textTheme.copyWith(
-                      bodyText1: TextStyle(
-                      fontSize: 18 * curScaleFactor,
-                      color: Color.fromRGBO(42, 47, 53, 1),
-                      ),
-                      bodyText2: TextStyle(
-                      fontSize: 14 * curScaleFactor,
-                      color: Color.fromRGBO(42, 47, 53, 1),
-                      ) ,
-                      headline1: TextStyle(
+                  bodyText1: TextStyle(
+                    fontSize: 18 * curScaleFactor,
+                    color: Color.fromRGBO(42, 47, 53, 1),
+                  ),
+                  bodyText2: TextStyle(
+                    fontSize: 14 * curScaleFactor,
+                    color: Color.fromRGBO(42, 47, 53, 1),
+                  ),
+                  headline1: TextStyle(
                       fontSize: 24 * curScaleFactor,
                       color: Color.fromRGBO(42, 47, 53, 1),
-                      fontWeight: FontWeight.bold
-                      ),
-                      headline2: TextStyle(color: Color.fromRGBO(109, 131, 182, 1),fontSize: 18 * curScaleFactor,fontWeight: FontWeight.bold)
-                      ))
-                      ,
+                      fontWeight: FontWeight.bold),
+                  headline2: TextStyle(
+                      color: Color.fromRGBO(56, 47, 56, 1),
+                      fontSize: 18 * curScaleFactor,
+                      fontWeight: FontWeight.bold))),
           builder: (context, child) {
             return Directionality(
                 textDirection: TextDirection.rtl, child: child);
@@ -59,20 +68,15 @@ class MainPageState extends State<MainPage> {
           home: Scaffold(
             appBar: appBar,
             body: SafeArea(
-                child: Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-                      height: (mediaQuery.size.height -
-                              (mediaQuery.padding.top +
-                                  appBar.preferredSize.height + mediaQuery.padding.bottom )) *
-                          0.80,
+                      height: actualScreenSize * (1-counterPtg),
                       child: Praying()),
                   Container(
-                      height: (mediaQuery.size.height -
-                              (mediaQuery.padding.top +
-                                  appBar.preferredSize.height + mediaQuery.padding.bottom)) *
-                          0.20,
+                      height: actualScreenSize *
+                          counterPtg,
                       child: Counting()),
                 ],
               ),
